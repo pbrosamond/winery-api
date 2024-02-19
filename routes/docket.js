ab1cb51ba731f62eee6956b5b9efe6a8230f922b
pbrosamond <pbrosamond@gmail.com>
2024-02-19 12:05:07 -0800

    updated docket post request

--- routes/docket.js
+++ routes/docket.js
@@ -28,52 +28,55 @@ router
       ];
 
       // Validating the fields
-      const valResult = validatingFields(req.body, requiredfields);
+      const valResult = validatingFields(req.body, requiredFields);
 
       if (valResult.checkCode === 1) {
-        return res
-          .status(400)
-          .send(`Post body does not include ${valResult.field}`);
+        return res.status(400).send(`Post body does not include ${valResult.field}`);
       }
-
+  
       if (valResult.checkCode === 2) {
         return res.status(400).send(`${valResult.field} should not be empty`);
       }
-
-      // Check if the warehouse_id value exists in the warehouses table
+  
       const { vintage, grower, varietal, vineyard, block, row } = req.body;
-
-      if (!vintage && !grower && !varietal && !vineyard && !block && !row) {
+  
+      if (!vintage || !grower || !varietal || !vineyard || !block || !row) {
         return res.status(400).send(`Please fill all required docket fields`);
       }
-
-      // if (!(2000 <= vintage && vintage <= currentYear)) {
-      //   return res.status(400).send(`Please select a valid year`);
-      // }
-
+  
+      const currentYear = new Date().getFullYear();
+      
+      if (!(2000 <= vintage && vintage <= currentYear)) {
+        return res.status(400).send(`Please select a valid year`);
+      }
+  
+      // Generate docket_name
+      const docketName = `${vintage}${grower.substring(0, 2).toUpperCase()}${varietal.substring(0, 2).toUpperCase()}${vineyard.substring(0, 2).toUpperCase()}${block.toString().padStart(2, '0')}${row.toString().padStart(2, '0')}`;
+  
       // Insert new inventory item into the database
       const result = await database("dockets").insert({
+        docket_name: docketName,
         vintage,
         grower,
         varietal,
         vineyard,
         block,
-        row,
+        row
       });
-
+  
       const newDocket = result[0];
       const createdDocket = await database("dockets")
-        .select(["vintage", "grower", "varietal", "vineyard", "block", "row"])
+        .select(["docket_name", "vintage", "grower", "varietal", "vineyard", "block", "row"])
         .where({
           id: newDocket,
         })
         .first();
-
+  
       res.status(201).send(createdDocket);
     } catch (error) {
       res.status(500).send(`Error creating docket: ${error}`);
     }
-})
+  })
   .get('/', async (_req, res) => {
     try {
       const data = await database("dockets")
